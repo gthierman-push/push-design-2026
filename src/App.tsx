bdd3d3a448217b7c34036a85673b9f0de5cf4166
Geoff Thierman <88002852+gthierman-push@users.noreply.github.com>
2026-09-14 12:39:44 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-sans m-10 font-medium">Vite + React + Tailwind Starter</h1>
+    </>
+  );
+}
+
+export default App;
